a1c30ea26559984b835f14ce78cf28ddcad932d9
Bojo992 <mm22mm44bbi@gmail.com>
2025-03-04 15:39:24 +0000

    (っ °Д °;)っ

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-    output: 'export',
-    assetPrefix: isProd ? 'CS4227-Restaurant-Management-System' : '',
-    images: {
-        unoptimized: true,
-    },
-    trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
