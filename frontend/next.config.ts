ee1e2e196f809d630637751a1736e09476c91bec
Bojo992 <mm22mm44bbi@gmail.com>
2025-03-04 16:48:40 +0000

    I beg you to work ╰（‵□′）╯

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true, // Since Next.js Image Optimization doesn't work with static exports
-  },
-  assetPrefix: './',
   basePath: '/CS4227-Restaurant-Management-System',
 };
 
